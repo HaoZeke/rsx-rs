cb27b5d61d3f47feb256dce7cdc31866c639cc5d
Rohit Goswami <rgoswami@ieee.org>
2026-05-16 19:06:26 -0500

    fix: remove stale Sphinx static path
    Co-authored-by: ruhirg <ruhiresearcher209@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ templates_path = ["_templates"]
 exclude_patterns = []
 
 html_theme = "shibuya"
-html_static_path = ["_static"]
+html_static_path = []
 
 html_theme_options = {
     "github_url": "https://github.com/HaoZeke/rsx-rs",
